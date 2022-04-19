ae62533f63bb848dbd87afe990f2591021d3ef01
Nithya Mylakumar M <77951964+nithyamylakumar@users.noreply.github.com>
2022-04-19 15:44:09 +0530

    2nd and 3rd exercise

--- array.py
+++ array.py
@@ -29,3 +29,52 @@ print(expense)
 
 expense[3]= expense[3] - 200
 print(expense)
+
+
+# exercise 2
+
+heros=['spider man','thor','hulk','iron man','captain america']
+
+# 1. Length of the list
+
+length = len(heros)
+print("length of the list is",length)
+
+# 2. Add 'black panther' at the end of this list
+
+heros.append('black panther')
+print("updated list is", heros)
+
+# 3. You realize that you need to add 'black panther' after 'hulk',
+#   so remove it from the list first and then add it after 'hulk'
+
+heros.remove('black panther')
+heros.insert(3, 'black panther')
+print('Updated:',heros)
+
+# 4. Now you don't like thor and hulk because they get angry easily :)
+# So you want to remove thor and hulk from list and replace them with doctor strange (because he is cool).
+# Do that with one line of code.
+
+heros[1:3]=['doctor strange']
+print(heros)
+
+#5. Sort the heros list in alphabetical order (Hint. Use dir() functions to list down all functions available in list)
+
+heros.sort()
+print(heros)
+
+
+# exercise 3:
+# Create a list of all odd numbers between 1 and a max number. 
+# Max number is something you need to take from a user using input() function
+
+max = int(input('enter max no.: '))
+
+odd = []
+
+for i in range(max):
+    if i%2 != 0:
+        odd.append(i)
+
+print('Odd numbers:',odd)
